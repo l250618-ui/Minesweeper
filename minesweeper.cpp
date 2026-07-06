2058d2417997d0060c0c8cfbb0c5bd71bffaadf0
l250618-ui <l250618@lhr.nu.edu.pk>
2026-07-06 16:50:22 +0500

    Generated 4 random mines on 5x5 board

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 int main() {
-    return 0;
+    int board[5][5];
+
+    // initialize
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            board[i][j] = 0;
+        }
+    }
+
+    // generating random numbers
+    int row = rand() % 5;
+    int col = rand() % 5;
+    board[row][col] = -1;
+
+    // print
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
